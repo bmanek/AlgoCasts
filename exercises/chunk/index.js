bf9e61374f2de2c415af8b7d0795ff3cedfc0329
bmanek <41875460+bmanek@users.noreply.github.com>
2019-08-19 17:50:09 -0400

    add solution 1 and 2 to chunk

--- exercises/chunk/index.js
+++ exercises/chunk/index.js
@@ -8,6 +8,30 @@
 // chunk([1, 2, 3, 4, 5], 4) --> [[ 1, 2, 3, 4], [5]]
 // chunk([1, 2, 3, 4, 5], 10) --> [[ 1, 2, 3, 4, 5]]
 
-function chunk(array, size) {}
+// solution 1
+// function chunk(array, size) {
+//   const bigChunk = []
+//   for (let element of array) {
+//     const last = bigChunk[bigChunk.length - 1]
+//
+//     if (!last || last.length === size) {
+//       bigChunk.push([element])
+//     } else {
+//       last.push(element)
+//     }
+//   }
+//   return bigChunk
+// }
+
+// solution 2 (YAY IT WORKED!!!!)
+function chunk(array, size) {
+  const bigChunk = []
+  let index = 0
+  while (index < array.length) {
+    bigChunk.push(array.slice(index, index + size))
+    index += size
+  }
+  return bigChunk
+}
 
 module.exports = chunk;
