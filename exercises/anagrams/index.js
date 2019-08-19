4dc079303b3e748a6446f906dc333cfdef2f501b
bmanek <41875460+bmanek@users.noreply.github.com>
2019-08-19 19:09:03 -0400

    add solution 1 to anagrams

--- exercises/anagrams/index.js
+++ exercises/anagrams/index.js
@@ -7,7 +7,36 @@
 //   anagrams('rail safety', 'fairy tales') --> True
 //   anagrams('RAIL! SAFETY!', 'fairy tales') --> True
 //   anagrams('Hi there', 'Bye there') --> False
+//
 
-function anagrams(stringA, stringB) {}
+// Solution 1
+// function anagrams(stringA, stringB) {
+//   const aCharMap = buildCharMap(stringA)
+//   const bCharMap = buildCharMap(stringB)
+//
+//   if (Object.keys(aCharMap).length !== Object.keys(bCharMap).length) {
+//     return false
+//   }
+//   for (char in aCharMap) {
+//     if (aCharMap[char] !== bCharMap[char]) {
+//       return false
+//     }
+//   }
+//   return true
+// }
+//
+// function buildCharMap(str) {
+//   const charMap = {}
+//
+//   for (char of str.replace(/[^\w]/g, "").toLowerCase() ) {
+//     charMap[char] = charMap[char] + 1 || 1
+//   }
+//   return charMap
+// }
+
+// Solution 2
+function anagrams(stringA, stringB) {
+
+}
 
 module.exports = anagrams;
