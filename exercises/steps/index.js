102d3c3f37021302985a08c833f1848eedbd767d
bmanek <41875460+bmanek@users.noreply.github.com>
2019-08-20 11:06:16 -0400

    add iterative and recursive solutions to steps

--- exercises/steps/index.js
+++ exercises/steps/index.js
@@ -17,6 +17,51 @@
 //       '### '
 //       '####'
 
-function steps(n) {}
+// solution 1 (iterative)
+// function steps(n) {
+//   for (let row = 0; row < n; row++) {
+//     let stair = "";
+//
+//     for (let column = 0; column < n; column++) {
+//       if (column <= row) {
+//         stair += "#"
+//       } else {
+//         stair += " "
+//       }
+//     }
+//     console.log(stair)
+//   }
+// }
+
+// solution 2 (recursive)
+function steps(n, row = 0, stair = ''){
+  if (n === row) {
+    return;
+  }
+
+  if (n === stair.length) {
+    console.log(stair)
+    return steps(n, row + 1)
+  }
+
+  if (stair.length <= row) {
+    stair += "#"
+  } else {
+    stair += " "
+  }
+  steps(n, row, stair)
+}
+
 
 module.exports = steps;
+
+
+// print N lines. Each line has N total 'slots'. The number of the loop will
+// correspond to the number of hash marks.
+
+// first attempt
+// function steps(n) {
+//   for (let i = 1; i <= n; i++) {
+//     console.log("#" + Array(n - i).join(" ") )
+//   }
+// }
