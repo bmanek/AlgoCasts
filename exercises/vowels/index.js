2aae60dc7b0fad2f301aa40a902ff934609e8cc9
bmanek <41875460+bmanek@users.noreply.github.com>
2019-08-20 20:17:34 -0400

    add iterative solution to vowels

--- exercises/vowels/index.js
+++ exercises/vowels/index.js
@@ -8,18 +8,35 @@
 //   vowels('Why?') --> 0
 
 // initial effort (all tests passing!)
-function vowels(str) {
-  vowelCounter = 0
-  regex = RegExp(/[aeiou]/)
-    for (let char of str) {
-      if (regex.test(char.toLowerCase())) {
-        vowelCounter += 1
-      }
-    }
-  return vowelCounter
-}
+// function vowels(str) {
+//   vowelCounter = 0
+//   regex = RegExp(/[aeiou]/)
+//     for (let char of str) {
+//       if (regex.test(char.toLowerCase())) {
+//         vowelCounter += 1
+//       }
+//     }
+//   return vowelCounter
+// }
 
-// refactor/official
+// solution 1 (iterative)
+// function vowels(str) {
+//   let count = 0;
+//   const vowelList = ['a', 'e', 'i', 'o', 'u']
+//
+//   for (let char of str.toLowerCase() ) {
+//     if (vowelList.includes(char)) {
+//       count++
+//     }
+//   }
+//
+//   return count;
+// }
+
+// solution 2 (RegEx)
+function vowels(str) {
+  
+}
 
 
 module.exports = vowels;
