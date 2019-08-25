b9504344cd9bf6e5ede04fd221e62933b2224d8f
bmanek <41875460+bmanek@users.noreply.github.com>
2019-08-25 12:17:51 -0400

    add initial attempt and official solution to weave

--- exercises/weave/index.js
+++ exercises/weave/index.js
@@ -3,7 +3,7 @@
 // 2) Implement the 'weave' function.  Weave
 // receives two queues as arguments and combines the
 // contents of each into a new, third queue.
-// The third queue should contain the *alterating* content
+// The third queue should contain the *alternating* content
 // of the two queues.  The function should handle
 // queues of different lengths without inserting
 // 'undefined' into the new one.
@@ -24,6 +24,37 @@
 
 const Queue = require('./queue');
 
-function weave(sourceOne, sourceTwo) {}
+// initial attempt
+// Note: how do I confirm/test output?
+// function weave(sourceOne, sourceTwo) {
+//   const q = new Queue();
+//   while (sourceOne.length > 0 && sourceTwo.length > 0) {
+//     checkAndAdd(sourceOne, q)
+//     checkAndAdd(sourceTwo, q)
+//   }
+//   return q
+// }
+//
+// function checkAndAdd(source, target) {
+//   let last = source.peek()
+//   if (last) {
+//     target.add(source.remove())
+//   }
+// }
+
+//solution
+function weave(sourceOne, sourceTwo) {
+  const q = new Queue();
+  while (sourceOne.peek() || sourceTwo.peek()) {
+    if (sourceOne.peek() ) {
+      q.add(sourceOne.remove() )
+    }
+
+    if (sourceTwo.peek() ) {
+      q.add(sourceTwo.remove() )
+    }
+  }
+  return q
+}
 
 module.exports = weave;
