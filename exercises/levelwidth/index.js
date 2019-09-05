53aba337e1fb149cfcc1db2e4b1e5824cf21c8ec
bmanek <41875460+bmanek@users.noreply.github.com>
2019-09-05 15:43:24 -0400

    add solution to levelwidth

--- exercises/levelwidth/index.js
+++ exercises/levelwidth/index.js
@@ -11,6 +11,34 @@
 // 4       5
 // Answer: [1, 3, 2]
 
-function levelWidth(root) {}
+
+// traverseBF(fn) {
+//   const arr = [this.root]
+//   while (arr.length) {
+//     const node = arr.shift()
+//     arr.push(...node.children)
+//     fn(node)
+//   }
+// }
+
+// couldn't do the attempt
+
+function levelWidth(root) {
+  const arr = [root, "stop"]
+  const counters = [0]
+
+  while (arr.length > 1) {
+    const node = arr.shift()
+
+    if (node === "stop") {
+      counters.push(0)
+      arr.push("stop")
+    } else {
+      arr.push(...node.children)
+      counters[counters.length - 1]++
+    }
+  }
+  return counters
+}
 
 module.exports = levelWidth;
